957bf59e101afc906a9ed65a2d841847ddd9d225
patriciasmashp <patriciasmashlol@mail.ru>
2024-11-15 19:31:56 +0500

    initial

---
+++ dists/sinara/main.js
@@ -0,0 +1,12 @@
+const t = AOS.init({
+    once: true,
+});
+
+// var controller = new ScrollMagic.Controller();
+
+// // create a scene
+// gsap.to(window, {
+//     // scrollTrigger: 'header', // start the animation when ".box" enters the viewport (once)
+//     scrollTo: 400
+   
+// });
