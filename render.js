bc3d71ba6017e4cae44512351d9257eb48009a90
dan005 <danil.shevelev.2004@mail.ru>
2024-11-23 02:35:54 +0500

    [fix] tabs

--- render.js
+++ render.js
@@ -13,71 +13,71 @@ const _personTemp = fs.readFileSync(path.resolve(__dirname, 'src/person.mustache
 const _trainsTemp = fs.readFileSync(path.resolve(__dirname, 'src/trains.mustache'), 'utf-8');
 
 function renderHome() {
-	return _renderWrapper(
-		'Главная',
-		Mustache.render(_homeTemp, {}),
-	);
+    return _renderWrapper(
+        'Главная',
+        Mustache.render(_homeTemp, {}),
+    );
 }
 
 function renderPersons(persons) {
-	return _renderWrapper(
-		'Книга почета',
-		_renderCatalog(
-			Mustache.render(_personsTemp, {
-				persons,
-			}),
-		),
-	);
+    return _renderWrapper(
+        'Книга почета',
+        _renderCatalog(
+            Mustache.render(_personsTemp, {
+                persons,
+            }),
+        ),
+    );
 }
 
 function renderTrains(trains) {
-	return _renderWrapper(
-		'Дорога времени',
-		_renderCatalog(
-			Mustache.render(_trainsTemp, {
-				trains,
-			}),
-		),
-	);
+    return _renderWrapper(
+        'Дорога времени',
+        _renderCatalog(
+            Mustache.render(_trainsTemp, {
+                trains,
+            }),
+        ),
+    );
 }
 
 function _renderCatalog(contentBody) {
-	return _renderWithSidebar(
-		Mustache.render(_catalogTemp, {
-			contentBody,
-		}),
-	);
+    return _renderWithSidebar(
+        Mustache.render(_catalogTemp, {
+            contentBody,
+        }),
+    );
 }
 
 function renderPerson(person, personsLength) {
-	return _renderWrapper(
-		person.heading,
-		_renderWithSidebar(
-			Mustache.render(_personTemp, {
-				...person,
-				prevId: person.id > 1 ? person.id - 1: null,
+    return _renderWrapper(
+        person.heading,
+        _renderWithSidebar(
+            Mustache.render(_personTemp, {
+                ...person,
+                prevId: person.id > 1 ? person.id - 1: null,
                 nextId: person.id < personsLength ? person.id + 1 : null,
-			}),
-		),
-	);
+            }),
+        ),
+    );
 }
 
 function _renderWithSidebar(content) {
-	return Mustache.render(_withSidebarTemp, {
-		content,
-	});
+    return Mustache.render(_withSidebarTemp, {
+        content,
+    });
 }
 
 function _renderWrapper(title, main) {
-	return Mustache.render(_wrapperTemp, {
-		title, 
-		main,
-	});
+    return Mustache.render(_wrapperTemp, {
+        title, 
+        main,
+    });
 }
 
 module.exports = {
-	renderHome,
-	renderPersons,
-	renderPerson,
-	renderTrains,
+    renderHome,
+    renderPersons,
+    renderPerson,
+    renderTrains,
 }
