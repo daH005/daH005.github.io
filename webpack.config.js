92711904d92d750ddad52a68781bc384fb40b7e8
dan005 <danil.shevelev.2004@mail.ru>
2024-11-23 00:30:05 +0500

    made ids of persons, pagination

--- webpack.config.js
+++ webpack.config.js
@@ -12,12 +12,14 @@ module.exports = {
         filename: 'bundle.js', // Имя выходного файла сборки
         path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
     },
+
     devServer: {
         contentBase: path.join(__dirname, 'dist'), // путь к папке с вашими файлами
         compress: true,
         port: 9000, // порт для сервера
         hot: true // горячая замена модулей
-      },
+    },
+
     module: {
         rules: [
             {
@@ -38,35 +40,45 @@ module.exports = {
     plugins: [
         new CopyPlugin({
             patterns: [
-              { from: path.resolve(__dirname, 'src/dists/images'), to: path.resolve(__dirname, 'dist/dists/images') },
+                { from: path.resolve(__dirname, 'src/dists/images'), to: path.resolve(__dirname, 'dist/dists/images') },
             ],
-          }),
-        ...dataLoader.person.map(item => {
-        return new HtmlWebpackPlugin({
-            template: './src/sample/person.mustache',
-            filename: `pages/${item.heading.replace(" ", "_").split(" ")[0]}.html`,
-            templateParameters: item,
-        });
-    }),
-    new HtmlWebpackPlugin({
-        template: './src/index.html'
-    }),
-    new HtmlWebpackPlugin({
-        template: './src/road.mustache',
-        filename: 'road.html',
-        templateParameters: {
-            sidebar: templateRender.getSidebarTemplate(),
-            trains: dataLoader.trains 
-        }
-        
-    }),
-    new HtmlWebpackPlugin({
-        template: './src/list.html',
-        filename: 'list.html'
-    })
-],
+        }),
 
-    devServer: {
+        ...dataLoader.person.map(item => {
+            return new HtmlWebpackPlugin({
+                template: './src/sample/person.mustache',
+                filename: `pages/${item.id}.html`,
+                templateParameters: {
+                    ...item,
+                    prevId: item.id > 1 ? item.id : null,
+                    nextId: item.id < dataLoader.person.length ? item.id + 1 : null,
+                },
+            });
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './src/road.mustache',
+            filename: 'road.html',
+            templateParameters: {
+                sidebar: templateRender.getSidebarTemplate(),
+                trains: dataLoader.trains 
+            },
+        }),
+
+        new HtmlWebpackPlugin({
+            template: './src/list.mustache',
+            filename: 'list.html',
+            templateParameters: {
+                person: dataLoader.person,
+            },
+        }),
+    ],
+
+    devServer: {  // ToDo: Два ключа "devServer"? Выходит этот переопределяет первый? Или я чего-то не понимаю?
         static: {
             directory: path.join(__dirname, 'dist'), // Каталог для статики
         },
